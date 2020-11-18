22eb850e2f98d61c63fe26414bd512d4cb61010b
Marco <marcocobos15@gmail.com>
2020-11-18 11:29:17 -0500

    Primer commit

---
+++ src/Aplicacion.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		JOptionPane.showMessageDialog(null, "Hola Mundo");
+	}
+
+}
